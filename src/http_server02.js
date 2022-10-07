111601b6c52e33b1405acaee5183d0d5fe239bec
Huang-Jian-Rong <lvsonylg@gmail.com>
2022-10-07 14:05:06 +0800

    簡易server web

--- src/http_server02.js
+++ src/http_server02.js
@@ -3,14 +3,14 @@ const fs = require('fs').promises;
 
 const server = http.createServer((req, res) => {
 
-    fs.writeFile(__dirname + '/headers.txt', JSON.stringify(req.headers)).then(data=>{
-        console.log({data});
+    fs.writeFile(__dirname + '/headers.txt', JSON.stringify(req.headers)).then(data => {
+        console.log({ data });
         // __dirname + '/headers.txt' 建立檔案txt
         // JSON.stringify(req.headers) 
         // then(data=> 錯誤情況
 
         res.writeHead(200, {
-        'Content-Type': 'text/html'
+            'Content-Type': 'text/html'
         });
 
         res.end('<h2>abcde</h2>')
@@ -22,5 +22,5 @@ const server = http.createServer((req, res) => {
 
 
 });
- 
+
 server.listen(3000);
