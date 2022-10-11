feac3dd4cf6db7176e754e0c4a2b09ab22c9b8d3
Huang-Jian-Rong <lvsonylg@gmail.com>
2022-10-11 10:25:47 +0800

    安裝bootstrap

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(express.static('public'));
 //靜態資料夾
 // app.use(express.static(__dirname + '/public'));
+// !比較精準的路徑方法
+
+app.use(express.static('node_modules/bootstrap/dist'));
+// 會視為第2個根目錄
+
 
 app.get('/', (req, res)=>{
     res.send(`<h2>你好</h2>`);
@@ -22,8 +27,8 @@ app.use((req, res)=>{
 
 
 const port = process.env.SERVER_PORT || 3002;
-app.listen(3000, ()=>{
-    console.log(`server started, PORT: ${port}`);
+app.listen(port, () => {
+    console.log(`server started, port: ${port}`);
 });
 
 // 之後都會用express
