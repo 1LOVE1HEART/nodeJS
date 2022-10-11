90c135f7f9480cb048e9f6a26c487661bb09cabb
Huang-Jian-Rong <lvsonylg@gmail.com>
2022-10-11 10:01:55 +0800

    靜態資料夾設定

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
+
+app.use(express.static('public'));
+//靜態資料夾
+// app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res)=>{
     res.send(`<h2>你好</h2>`);
 });
 
+app.get('/abc', (req, res)=>{
+    res.send(`<h2>abc</h2>`);
+});
+
+app.use((req, res)=>{
+    res.type('text/plain');//純文字
+    res.status(404).send('找不到頁面');
+});
+
+
+const port = process.env.SERVER_PORT || 3002;
 app.listen(3000, ()=>{
-    console.log('server started');
+    console.log(`server started, PORT: ${port}`);
 });
 
 // 之後都會用express
