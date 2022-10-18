a647c62a1f12a8675233b6b892168db0f3eaf0e6
Huang-Jian-Rong <lvsonylg@gmail.com>
2022-10-18 13:26:53 +0800

    新增資料

--- routes/address-book.js
+++ routes/address-book.js
@@ -14,7 +14,35 @@ router.get('/add', async (req, res)=>{
     res.render('address-book/add')
 });
 router.post('/add', upload.none(), async (req, res)=>{
-    res.json(req.body);
+    // res.json(req.body);
+    const output = {
+        success: false,
+        code: 0,
+        error: {},
+        postData: req.body, // 除錯用
+    };
+
+    // TODO: 檢查欄位的格式, 可以用 joi npm套件
+
+    const sql = "INSERT INTO `address_book`( `name`, `email`, `mobile`, `birthday`, `address`, `created_at`) VALUES (?,?,?,?,?, NOW())";
+
+    const [result] = await db.query(sql, [
+        req.body.name,
+        req.body.email,
+        req.body.mobile,
+        req.body.birthday || null, 
+        req.body.address,
+    ]);
+
+    if(result.affectedRows) output.success = true;
+
+    res.json(output);
+
+});
+
+
+router.get('/item/:id', async (req, res)=>{
+    // 讀取單筆資料
 });
 
 
